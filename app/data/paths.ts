a612c824f621754a1a0c09bf1e2a2a735341d04e
Dominic Chen <dominic.chen630@gmail.com>
2025-03-18 15:13:36 -0400

    worked for a bit but broken now

--- app/data/paths.ts
+++ app/data/paths.ts
@@ -5,7 +5,8 @@ const paths = {
     meFr: '/me-fr.jpeg',
     tmu: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/TMU_logo.svg/1200px-TMU_logo.svg.png',
     fgf: 'https://www.fgfbrands.com/wp-content/uploads/2021/09/FGF-Brands-Logo.png',
-    ecodex: 'https://github.com/josephleungg/EcoDex/raw/main/client/public/images/ecodex-trans-readme.png?raw=true'
+    ecodex: 'https://github.com/josephleungg/EcoDex/raw/main/client/public/images/ecodex-trans-readme.png?raw=true',
+    loader: '/loader.gif'
 }
 
 export default paths;
