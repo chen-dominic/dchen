cd8824b975f3dc3288d2b54131b5635a68444d3a
Dominic Chen <dominic.chen630@gmail.com>
2025-03-18 15:31:03 -0400

    fixed email send

--- app/sections/contact.tsx
+++ app/sections/contact.tsx
@@ -27,10 +27,9 @@ export default function Contact() {
     setIsSending(true);
     setError(null);
 
-    const serviceID = process.env.EMAILJS_SERVICE_ID || '';
-    const templateID = process.env.EMAILJS_TEMPLATE_ID || '';
-    const publicKey = process.env.EMAILJS_PUBLIC_KEY || '';
-
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || '';
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || '';
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || '';
     try{
         const templateParams = {
             name: formData.name,
